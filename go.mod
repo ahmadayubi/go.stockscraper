74c92610a3c66d782dc387d1e88bc580ab4d5520
Ahmad Ayubi <aayub041@uottawa.ca>
2021-05-07 13:22:29 -0400

    Add request tracking

--- go.mod
+++ go.mod
@@ -12,8 +12,10 @@ require (
 	github.com/gocolly/colly v1.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
 	github.com/jawher/mow.cli v1.2.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	golang.org/x/net v0.0.0-20210331212208-0fccb6fa2b5c // indirect
--- server.go
+++ server.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
+	_ "github.com/jackc/pgx/stdlib"
 	"go.stockscraper/reddit"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
+	"time"
 )
 
 var subreddits = []string{"wallstreetbets", "pennystocks", "CanadianInvestor", "StockMarket", "Daytrading", "stocks"}
 var sorts = []string{"hot", "new", "top"}
+const insertSQL = "INSERT INTO requests(date, ip, requested_limit) VALUES($1, $2, $3)"
+var DB *sql.DB
+var connectedToDatabase = true
 
 func main(){
+	db, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Print("Database not connected")
+		connectedToDatabase = false
+	} else {
+		if err = db.Ping(); err != nil {
+			log.Print("Lost connection to database")
+			connectedToDatabase = false
+		} else {
+			DB = db
+		}
+	}
+
 	http.HandleFunc("/", serveIndex)
 	http.HandleFunc("/json", getJson)
 	log.Println("Running...")
@@ -69,10 +88,30 @@ func serveIndex (w http.ResponseWriter, r *http.Request){
 		limit = 10
 	}
 
+	if connectedToDatabase {
+		ip := getIP(r)
+		go func(limit int, ip string) {
+			t := time.Now()
+			query, err := DB.Prepare(insertSQL)
+			defer query.Close()
+			if err == nil {
+				_, err = query.Exec(t.Format("2006-01-02"), ip, limit)
+			}
+		}(limit, ip)
+	}
+
 	tmpl := template.Must(template.ParseFiles("templates/index.html"))
 	err = tmpl.Execute(w, limit)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+}
+
+func getIP(r *http.Request) string {
+	forwarded := r.Header.Get("X-FORWARDED-FOR")
+	if forwarded != "" {
+		return forwarded
+	}
+	return r.RemoteAddr
 }
